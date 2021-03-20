7b7ec0147dead7dc52f3a7debf0e23e3d76f261c
Nicholas Krämer <nicokraemer@gmx.net>
2021-03-20 12:38:36 +0100

    save figure

--- visualization/firstattempt.py
+++ visualization/firstattempt.py
@@ -17,7 +17,7 @@ results_nci = pd.read_csv(
     "data/workprecision_first_attempt_bratus_nci.csv", index_col=0
 )
 
-fig, ax = plt.subplots(ncols=3, figsize=(9, 3), dpi=200, tight_layout=True)
+fig, ax = plt.subplots(ncols=3, figsize=(12, 4), dpi=200, tight_layout=True)
 
 
 for colidx in results_rmse.columns:
@@ -25,15 +25,18 @@ for colidx in results_rmse.columns:
     ax[1].loglog(results_anees.index, results_anees[colidx], marker="o", label=colidx)
     ax[2].semilogx(results_nci.index, results_nci[colidx], marker="o", label=colidx)
 
+
+ax[1].fill_between(
+    results_anees.index, out[0], out[1], color="green", alpha=0.25, label="99% Conf."
+)
+
+
 for axis in ax:
     axis.set_xlabel("N")
     axis.legend()
 
-ax[1].fill_between(
-    results_anees.index, out[0], out[1], color="green", alpha=0.25, label="good"
-)
-
 ax[0].set_title("RMSE")
 ax[1].set_title("ANEES")
 ax[2].set_title("NCI")
+plt.savefig("figures/bratus_results.pdf")
 plt.show()
