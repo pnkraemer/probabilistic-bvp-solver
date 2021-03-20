77460c8afb136df8b2546afe5eb211485157e9e2
Nicholas Krämer <nicokraemer@gmx.net>
2021-03-20 13:06:05 +0100

    got started on calibratioN

--- bvps/_probnum_overwrites.py
+++ bvps/_probnum_overwrites.py
@@ -2,8 +2,10 @@
 
 from probnum import statespace, filtsmooth
 import numpy as np
+import scipy.linalg
 
-__all__ = ["from_ode"]
+__all__ = ["from_ode", "MyKalman"]
+
 
 def from_ode(ode, prior):
 
@@ -22,7 +24,7 @@ def from_ode(ode, prior):
 
     discrete_model = statespace.DiscreteGaussian(
         input_dim=prior.dimension,
-        output_dim = spatialdim,
+        output_dim=spatialdim,
         state_trans_fun=dyna,
         proc_noise_cov_mat_fun=diff_cholesky,
         jacob_state_trans_fun=jacobian,
@@ -33,3 +35,101 @@ def from_ode(ode, prior):
         forward_implementation="sqrt",
         backward_implementation="sqrt",
     )
+
+
+class MyKalman(filtsmooth.Kalman):
+    """Kalman filtering with calibration"""
+
+    def filter(
+        self,
+        dataset: np.ndarray,
+        times: np.ndarray,
+        _previous_posterior=None,
+    ):
+        """Apply Gaussian filtering (no smoothing!) to a data set.
+
+        Parameters
+        ----------
+        dataset : array_like, shape (N, M)
+            Data set that is filtered.
+        times : array_like, shape (N,)
+            Temporal locations of the data points.
+            The zeroth element in times and dataset is the location of the initial random variable.
+        _previous_posterior: KalmanPosterior
+            If specified, approximate Gaussian filtering and smoothing linearises at this, prescribed posterior.
+            This is used for iterated filtering and smoothing. For standard filtering, this can be ignored.
+
+        Returns
+        -------
+        KalmanPosterior
+            Posterior distribution of the filtered output
+        """
+        dataset, times = np.asarray(dataset), np.asarray(times)
+        rvs = []
+        sigmas = []
+
+        _linearise_update_at = (
+            None if _previous_posterior is None else _previous_posterior(times[0])
+        )
+        filtrv, *_ = self.update(
+            data=dataset[0],
+            rv=self.initrv,
+            time=times[0],
+            _linearise_at=_linearise_update_at,
+        )
+
+        rvs.append(filtrv)
+        for idx in range(1, len(times)):
+            _linearise_predict_at = (
+                None
+                if _previous_posterior is None
+                else _previous_posterior(times[idx - 1])
+            )
+            _linearise_update_at = (
+                None if _previous_posterior is None else _previous_posterior(times[idx])
+            )
+
+            filtrv, info = self.filter_step(
+                start=times[idx - 1],
+                stop=times[idx],
+                current_rv=filtrv,
+                data=dataset[idx],
+                _linearise_predict_at=_linearise_predict_at,
+                _linearise_update_at=_linearise_update_at,
+            )
+
+            sigma = info["info_upd"]["current_sigma"]
+            sigmas.append(sigma)
+            print(sigma)
+
+            rvs.append(filtrv)
+
+        ssq = np.mean(sigmas)
+        print("Warning: what about IEKF with the update?")
+        print("global sigma", ssq)
+        print()
+        return filtsmooth.FilteringPosterior(times, rvs, self.dynamics_model)
+
+    def update(self, rv, time, data, _linearise_at=None):
+
+        meas_rv, _ = self.measurement_model.forward_rv(
+            rv, t=time, _linearise_at=_linearise_at, compute_gain=True
+        )
+        # upd_rv, info2 = self.measurement_model.backward_realization(
+        #     data,
+        #     rv,
+        #     t=time,
+        #     _linearise_at=_linearise_at,
+        #     forwarded_rv=meas_rv,
+        #     gain=info["gain"],
+        # )
+        upd_rv, info = self.measurement_model.backward_realization(
+            data, rv, t=time, _linearise_at=_linearise_at
+        )
+
+        sigma = meas_rv.mean.T @ scipy.linalg.cho_solve(
+            (meas_rv.cov_cholesky, True), meas_rv.mean
+        )
+        info["current_sigma"] = sigma
+
+        return upd_rv, info
