bfa3332a49e7bb42093b632050f6b5a629d47aa8
Nicholas Krämer <nicokraemer@gmx.net>
2021-03-20 12:36:58 +0100

    more visualization

--- visualization/firstattempt.py
+++ visualization/firstattempt.py
@@ -2,15 +2,38 @@
 import pandas as pd
 
 import matplotlib.pyplot as plt
-results = pd.read_csv("data/workprecision_first_attempt_bratus.csv", index_col=0)
+from probnumeval.timeseries import chi2_confidence_intervals
 
 
-for colidx in results.columns:
-    plt.loglog(results.index, results[colidx], marker="o", label=colidx)
+out = chi2_confidence_intervals(dim=2)
+print(out)
+results_rmse = pd.read_csv(
+    "data/workprecision_first_attempt_bratus_rmse.csv", index_col=0
+)
+results_anees = pd.read_csv(
+    "data/workprecision_first_attempt_bratus_anees.csv", index_col=0
+)
+results_nci = pd.read_csv(
+    "data/workprecision_first_attempt_bratus_nci.csv", index_col=0
+)
 
-plt.xlabel("N")
-plt.ylabel("RMSE")
-plt.legend()
+fig, ax = plt.subplots(ncols=3, figsize=(9, 3), dpi=200, tight_layout=True)
+
+
+for colidx in results_rmse.columns:
+    ax[0].loglog(results_rmse.index, results_rmse[colidx], marker="o", label=colidx)
+    ax[1].loglog(results_anees.index, results_anees[colidx], marker="o", label=colidx)
+    ax[2].semilogx(results_nci.index, results_nci[colidx], marker="o", label=colidx)
+
+for axis in ax:
+    axis.set_xlabel("N")
+    axis.legend()
+
+ax[1].fill_between(
+    results_anees.index, out[0], out[1], color="green", alpha=0.25, label="good"
+)
+
+ax[0].set_title("RMSE")
+ax[1].set_title("ANEES")
+ax[2].set_title("NCI")
 plt.show()
-print(results.columns)
-print(results.index)
