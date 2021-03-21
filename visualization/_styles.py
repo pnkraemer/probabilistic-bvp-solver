f11c62a3676889bea22e3400267a306af337c2b0
Nicholas Krämer <nicokraemer@gmx.net>
2021-03-21 19:56:08 +0100

    some visualisation

---
+++ visualization/_styles.py
@@ -0,0 +1,4 @@
+"""Some common themes for plots."""
+
+LINESTYLES = ["dashed", "dotted", "solid"]
+MARKERS = ["^", "d", "s", "o"]
--- visualization/r_example.py
+++ visualization/r_example.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 from probnumeval.timeseries import chi2_confidence_intervals
-
+from _styles import LINESTYLES, MARKERS
 
 out = chi2_confidence_intervals(dim=2)
 print(out)
@@ -28,11 +28,28 @@ results_nci2 = pd.read_csv(
     "data/workprecision_first_attempt_r_example_nci_q3.csv", index_col=0
 )
 
-fig, ax = plt.subplots(ncols=3, figsize=(12, 4), dpi=200, tight_layout=True)
+
+plt.style.use(
+    [
+        "stylesheets/hollow_markers.mplstyle",
+        "stylesheets/8pt.mplstyle",
+        "stylesheets/13_tile_jmlr.mplstyle",
+        "stylesheets/thin_lines.mplstyle",
+        "stylesheets/probnumeval_colors.mplstyle",
+    ]
+)
+
+fig, ax = plt.subplots(ncols=4, dpi=200, constrained_layout=True)
 
 
-for colidx in results_rmse.columns[:1]:
-    ax[0].loglog(results_rmse.index, results_rmse[colidx], marker="o", label="q=4")
+for colidx, linestyle, marker in zip(results_rmse.columns[:1], LINESTYLES, MARKERS):
+    ax[0].loglog(
+        results_rmse.index,
+        results_rmse[colidx],
+        label="q=4",
+        linestyle=linestyle,
+        marker=marker,
+    )
     ax[1].loglog(results_anees.index, results_anees[colidx], marker="o", label="q=4")
     ax[2].semilogx(results_nci.index, results_nci[colidx], marker="o", label="q=4")
 
