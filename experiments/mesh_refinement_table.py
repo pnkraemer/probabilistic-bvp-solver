9af23ccae1f44b57427ebcbe5da1696a33382195
Nicholas Krämer <nicokraemer@gmx.net>
2021-05-11 09:47:44 +0200

    more mesh refinement

---
+++ experiments/mesh_refinement_table.py
@@ -0,0 +1,163 @@
+"""Try out probsolve_bvp."""
+import numpy as np
+import warnings
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from probnum import statespace, randvars, filtsmooth, diffeq
+from probnum._randomvariablelist import _RandomVariableList
+from bvps import bvp_solver, problem_examples, ode_measmods
+from tqdm import tqdm
+import pandas as pd
+
+import itertools
+from probnum import random_variables as randvars
+
+from probnumeval import timeseries
+
+
+
+bvp = problem_examples.problem_24_second_order(xi=2.5e-2)
+ibm = statespace.IBM(
+    ordint=3,
+    spatialdim=bvp.dimension,
+    forward_implementation="sqrt",
+    backward_implementation="sqrt",
+)
+measmod = ode_measmods.from_second_order_ode(bvp, ibm)
+
+
+# Solver and solver parameters
+solver = bvp_solver.BVPSolver.from_default_values(
+    ibm, use_bridge=True, initial_sigma_squared=1e2
+)
+MAXIT = 5
+
+# Plotting parameters
+t = np.linspace(bvp.t0, bvp.tmax, 200)
+titles = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+LOG_YLIM = (1e-12, 1e5)
+plt.style.use(
+    [
+        "./visualization/stylesheets/science.mplstyle",
+        "./visualization/stylesheets/misc/grid.mplstyle",
+        "./visualization/stylesheets/color/high-contrast.mplstyle",
+        "./visualization/stylesheets/8pt.mplstyle",
+    ]
+)
+mpl.rcParams['lines.linewidth'] = 0.5
+
+
+
+# Reference solution
+TOL = 1e-5
+initial_grid = np.linspace(bvp.t0, bvp.tmax, 150)
+initial_guess = np.ones((len(initial_grid), bvp.dimension))
+solution_gen = solver.solution_generator(
+    bvp,
+    atol=TOL,
+    rtol=TOL,
+    initial_grid=initial_grid,
+    initial_guess=None,
+    maxit_ieks=MAXIT,
+    maxit_em=1,
+    yield_ieks_iterations=False,
+)
+# Skip initialisation
+next(solution_gen)
+
+# First iteration
+reference_posterior, _ = next(solution_gen)
+
+
+
+
+
+
+
+
+
+
+
+N = 32
+
+for TOL in [1e-1, 1e-2, 1e-3]:
+    initial_grid = np.linspace(bvp.t0, bvp.tmax, N)
+    initial_guess = np.ones((len(initial_grid), bvp.dimension))
+    solver = bvp_solver.BVPSolver.from_default_values_std_refinement(
+        ibm, use_bridge=True, initial_sigma_squared=1e2, normalise_with_interval_size=False
+    )
+    solution_gen = solver.solution_generator(
+        bvp,
+        atol=TOL,
+        rtol=TOL,
+        initial_grid=initial_grid,
+        initial_guess=None,
+        maxit_ieks=MAXIT,
+        maxit_em=1,
+        yield_ieks_iterations=False,
+    )
+    # Skip initialisation
+    for i, (kalman_posterior, sigma_squared) in enumerate(solution_gen):
+        print(i, ":", len(kalman_posterior.locations))
+        if i > 5:
+            print("fail")
+            break
+
+    refsol = lambda x: reference_posterior(x).mean[:, 0]
+    approxsol = lambda x: kalman_posterior(x).mean[:, 0]
+    rmse = timeseries.root_mean_square_error(approxsol, refsol, t)
+    print("STD", TOL, rmse, len(kalman_posterior.locations))
+
+    solver = bvp_solver.BVPSolver.from_default_values(
+        ibm, use_bridge=True, initial_sigma_squared=1e2, normalise_with_interval_size=False
+    )
+    solution_gen = solver.solution_generator(
+        bvp,
+        atol=TOL,
+        rtol=TOL,
+        initial_grid=initial_grid,
+        initial_guess=None,
+        maxit_ieks=MAXIT,
+        maxit_em=1,
+        yield_ieks_iterations=False,
+    )
+    # Skip initialisation
+    for i, (kalman_posterior, sigma_squared) in enumerate(solution_gen):
+        print(i, ":", len(kalman_posterior.locations))
+        if i > 5:
+            print("fail")
+            break
+
+    refsol = lambda x: reference_posterior(x).mean[:, 0]
+    approxsol = lambda x: kalman_posterior(x).mean[:, 0]
+    rmse = timeseries.root_mean_square_error(approxsol, refsol, t)
+    print("Res", TOL, rmse, len(kalman_posterior.locations))
+
+
+
+
+
+    solver = bvp_solver.BVPSolver.from_default_values_probabilistic_refinement(
+        ibm, use_bridge=True, initial_sigma_squared=1e2, normalise_with_interval_size=False
+    )
+    solution_gen = solver.solution_generator(
+        bvp,
+        atol=TOL,
+        rtol=TOL,
+        initial_grid=initial_grid,
+        initial_guess=None,
+        maxit_ieks=MAXIT,
+        maxit_em=1,
+        yield_ieks_iterations=False,
+    )
+    # Skip initialisation
+    for i, (kalman_posterior, sigma_squared) in enumerate(solution_gen):
+        print(i, ":", len(kalman_posterior.locations))
+        if i > 5:
+            print("fail")
+            break
+
+    refsol = lambda x: reference_posterior(x).mean[:, 0]
+    approxsol = lambda x: kalman_posterior(x).mean[:, 0]
+    rmse = timeseries.root_mean_square_error(approxsol, refsol, t)
+    print("PRes", TOL, rmse, len(kalman_posterior.locations))
--- experiments/show_mesh_refinement.py
+++ experiments/show_mesh_refinement.py
@@ -15,7 +15,7 @@ from probnum import random_variables as randvars
 
 bvp = problem_examples.problem_24_second_order(xi=1e-2)
 ibm = statespace.IBM(
-    ordint=4,
+    ordint=3,
     spatialdim=bvp.dimension,
     forward_implementation="sqrt",
     backward_implementation="sqrt",
@@ -25,7 +25,7 @@ measmod = ode_measmods.from_second_order_ode(bvp, ibm)
 
 # Solver and solver parameters
 solver = bvp_solver.BVPSolver.from_default_values(
-    ibm, use_bridge=True, initial_sigma_squared=1e2
+    ibm, use_bridge=True, initial_sigma_squared=1e2, normalise_with_interval_size=False
 )
 MAXIT = 5
 TOL = 1e-6
@@ -33,22 +33,55 @@ TOL = 1e-6
 # Plotting parameters
 t = np.linspace(bvp.t0, bvp.tmax, 200)
 titles = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-LOG_YLIM = (1e-12, 1e4)
+LOG_YLIM = (1e-12, 1e5)
 plt.style.use(
     [
         "./visualization/stylesheets/science.mplstyle",
         "./visualization/stylesheets/misc/grid.mplstyle",
-        # "./visualization/stylesheets/color/high-contrast.mplstyle",
+        "./visualization/stylesheets/color/high-contrast.mplstyle",
         "./visualization/stylesheets/8pt.mplstyle",
-        "./visualization/stylesheets/one_of_13_tile.mplstyle",
-        # "./visualization/stylesheets/hollow_markers.mplstyle",
-        "./visualization/stylesheets/probnum_colors.mplstyle",
     ]
 )
+mpl.rcParams['lines.linewidth'] = 0.5
 
-for N in [2**5, 2**7, 2**9]:
+
+
+# Reference solution
+
+initial_grid = np.linspace(bvp.t0, bvp.tmax, 1500)
+initial_guess = np.ones((len(initial_grid), bvp.dimension))
+solution_gen = solver.solution_generator(
+    bvp,
+    atol=TOL,
+    rtol=TOL,
+    initial_grid=initial_grid,
+    initial_guess=None,
+    maxit_ieks=MAXIT,
+    maxit_em=1,
+    yield_ieks_iterations=False,
+)
+# Skip initialisation
+next(solution_gen)
+
+# First iteration
+reference_posterior, _ = next(solution_gen)
+
+
+
+
+
+
+
+
+
+
+
+
+for N in [2**10, 2**9, 2**8, 2**7, 2**6, 2**5, 2**4]:
 
     initial_grid = np.linspace(bvp.t0, bvp.tmax, N)
+    initial_guess = np.ones((len(initial_grid), bvp.dimension))
+
     solution_gen = solver.solution_generator(
         bvp,
         atol=TOL,
@@ -70,72 +103,50 @@ for N in [2**5, 2**7, 2**9]:
 
     # Set up all 7 subplots
     fig, ax = plt.subplots(
-        nrows=6,
+        nrows=5,
         sharex=True,
-        dpi=200,
-        figsize=(2, 3),
+        dpi=300,
+        figsize=(3,3),
         constrained_layout=True,
-        gridspec_kw={"height_ratios": [8, 8, 8, 8, 8, 8]},
+        gridspec_kw={"height_ratios": [8, 8, 8, 8, 1]},
     )
     idx = itertools.count()
     i = next(idx)
 
     # Evaluate the residual
     residual_evaluations = _RandomVariableList(
-        [measmod.forward_rv(rv, t)[0] for rv, t in zip(posterior_evaluations, t)]
+        [measmod.forward_rv(rv, t_)[0] for rv, t_ in zip(posterior_evaluations, t)]
     )
     residual_mean = residual_evaluations.mean
     residual_std = residual_evaluations.std * np.sqrt(sigma_squared)
 
     # Evaluate the reference solution and errors
-    reference_evaluated = kalman_posterior(t).mean[:, 0]
+    reference_evaluated = reference_posterior(t).mean[:, 0]
     SOL_YLIM = (1.5 * np.amin(reference_evaluated), np.amax(reference_evaluated) * 1.5)
-    # SOL_YLIM = (0.2, 1.6)
-    reference_color = "crimson"
+    reference_color = "black"
     reference_linestyle = "dotted"
     true_error = np.abs(reference_evaluated - posterior_mean[:, 0])
-    # true_error = np.zeros_like(t)
 
     # Extract plotting residuals and uncertainties
     error_estimates_std = np.abs(posterior_std[:, 0])
     error_estimates_res_mean = np.abs(residual_mean)
     error_estimates_res_std = np.abs(residual_std)
 
-    # Select grids to be plotted
-    grid_refine_via_std = kalman_posterior.locations  # dummy
-    grid_refine_via_prob_res = kalman_posterior.locations  # dummy
-    grid_refine_via_res = kalman_posterior.locations
-
-    # # First row: std-refinement grid
-    # for t in grid_refine_via_std:
-    #     ax[i].axvline(t, color="black", linewidth=0.5)
-    # ax[i].set_title(
-    #     f"$\\bf {titles[i]}$", loc="left", fontweight="bold", fontsize="x-small"
-    # )
-    # i = next(idx)
-
-    # Second row: uncertainties derived from std
-    ax[i].set_title(f"N: {len(kalman_posterior.locations)}")
-    # ax[i].semilogy(t, error_estimates_std, ":", color="black")
+    # First row: uncertainties derived from std
+    assert t.shape == error_estimates_std.shape
     ax[i].semilogy(t, error_estimates_std ** 2, "-", color="black")
-    ax[i].set_ylabel("$\\log \\mathbb{V}[Y_0](t)$", fontsize="x-small")
+    ax[i].fill_between(t, 1e-16*np.ones_like(t), error_estimates_std ** 2, color="black", alpha=0.2)
+    ax[i].set_ylabel("Log-Variance", fontsize="x-small")
     ax[i].semilogy(
         t,
         true_error ** 2,
         color=reference_color,
         linestyle=reference_linestyle,
     )
-    ax[i].axhline(TOL, color=reference_color)
-    ax[i].set_title(
-        f"$\\bf {titles[i]}$",
-        loc="left",
-        fontweight="bold",
-        fontsize="x-small",
-    )
     ax[i].set_ylim(LOG_YLIM)
     i = next(idx)
 
-    # Third row: Solution and uncertainties
+    # Next row: Solution and uncertainties
     ax[i].plot(
         t,
         reference_evaluated,
@@ -151,14 +162,11 @@ for N in [2**5, 2**7, 2**9]:
         color="black",
         linewidth=0,
     )
-    ax[i].set_ylabel("$Y_0(t)$", fontsize="x-small")
-    ax[i].set_title(
-        f"$\\bf {titles[i]}$", loc="left", fontweight="bold", fontsize="x-small"
-    )
+    ax[i].set_ylabel("Solution", fontsize="x-small")
     ax[i].set_ylim(SOL_YLIM)
     i = next(idx)
 
-    # Fourth row: Residual and uncertainties
+    # Next row: Residual and uncertainties
     ax[i].plot(
         t,
         residual_mean[:, 0],
@@ -178,45 +186,29 @@ for N in [2**5, 2**7, 2**9]:
         color="black",
         linewidth=0,
     )
-    ax[i].set_ylabel("$\\delta(Y)(t)$", fontsize="x-small")
-    ax[i].set_title(
-        f"$\\bf {titles[i]}$", loc="left", fontweight="bold", fontsize="x-small"
-    )
+    ax[i].set_ylabel("Residual", fontsize="x-small")
+
 
     i = next(idx)
 
-    # Fifth row: Log-residual and uncertainties
+    # Next row: Log-residual and uncertainties
+    assert t.shape == error_estimates_res_mean[:, 0].shape
     ax[i].semilogy(
         t,
         error_estimates_res_mean[:, 0] ** 2,
         color="black",
     )
-    # # ax[i].semilogy(
-    # #     t,
-    # #     error_estimates_res_mean[:, 0] + error_estimates_res_std[:, 0],
-    # #     color="black",
-    # #     linestyle="dashed",
-    # # )
-    ax[i].semilogy(
-        t,
-        true_error ** 2,
-        color=reference_color,
-        linestyle=reference_linestyle,
-    )
-    ax[i].set_ylim(LOG_YLIM)
-    ax[i].set_ylabel("$\\log \\mathbb{E}[\\delta(Y)](t)$", fontsize="x-small")
-    ax[i].set_title(
-        f"$\\bf {titles[i]}$", loc="left", fontweight="bold", fontsize="x-small"
-    )
-    ax[i].axhline(TOL, color=reference_color)
-    i = next(idx)
 
-    # Just the STD.
     ax[i].semilogy(
         t,
         error_estimates_res_mean[:, 0] ** 2 + error_estimates_res_std[:, 0] ** 2,
         color="black",
+        linestyle="dashed"
     )
+
+    ax[i].fill_between(t, 1e-16*np.ones_like(t), error_estimates_res_mean[:, 0] ** 2, color="black", alpha=0.2)
+    ax[i].fill_between(t, error_estimates_res_mean[:, 0] ** 2, error_estimates_res_mean[:, 0] ** 2 + error_estimates_res_std[:, 0] ** 2,color="blue", alpha=0.2)
+
     # # ax[i].semilogy(
     # #     t,
     # #     error_estimates_res_mean[:, 0] + error_estimates_res_std[:, 0],
@@ -225,69 +217,95 @@ for N in [2**5, 2**7, 2**9]:
     # # )
     ax[i].semilogy(
         t,
-        error_estimates_res_std[:, 0] ** 2,
-        color="black",
-    )
-    ax[i].semilogy(
-        t,
         true_error ** 2,
         color=reference_color,
         linestyle=reference_linestyle,
     )
     ax[i].set_ylim(LOG_YLIM)
-    ax[i].set_ylabel("$\\log \\mathbb{V}[\\delta(Y)](t)$", fontsize="x-small")
-    ax[i].set_title(
-        f"$\\bf {titles[i]}$", loc="left", fontweight="bold", fontsize="x-small"
-    )
-    ax[i].axhline(TOL, color=reference_color)
+    ax[i].set_ylabel("Log-Residual", fontsize="x-small")
     i = next(idx)
 
-    # Just the STD.
-    ax[i].semilogy(
-        t,
-        error_estimates_res_std[:, 0] ** 2,
-        color="black",
-    )
-    # # ax[i].semilogy(
-    # #     t,
-    # #     error_estimates_res_mean[:, 0] + error_estimates_res_std[:, 0],
-    # #     color="black",
-    # #     linestyle="dashed",
-    # # )
-    ax[i].semilogy(
-        t,
-        error_estimates_res_std[:, 0] ** 2,
-        color="black",
-    )
-    ax[i].semilogy(
-        t,
-        true_error ** 2,
-        color=reference_color,
-        linestyle=reference_linestyle,
-    )
-    ax[i].set_ylim(LOG_YLIM)
-    ax[i].set_ylabel("$\\log \\|\\delta(Y)(t)\\|$", fontsize="x-small")
-    ax[i].set_title(
-        f"$\\bf {titles[i]}$", loc="left", fontweight="bold", fontsize="x-small"
-    )
-    ax[i].axhline(TOL, color=reference_color)
-    i = next(idx)
+    for x in kalman_posterior.locations:
+        ax[i].axvline(x, color="black", linewidth=0.25)
 
-    # Sixth row: steps from refinement with residual only
-    # for t in grid_refine_via_res:
-    #     ax[i].axvline(t, color="black", linewidth=0.25)
+    #
+    # # Just the STD.
+    # ax[i].semilogy(
+    #     t,
+    #     error_estimates_res_mean[:, 0] ** 2 + error_estimates_res_std[:, 0] ** 2,
+    #     color="black",
+    # )
+    # # # ax[i].semilogy(
+    # # #     t,
+    # # #     error_estimates_res_mean[:, 0] + error_estimates_res_std[:, 0],
+    # # #     color="black",
+    # # #     linestyle="dashed",
+    # # # )
+    # ax[i].semilogy(
+    #     t,
+    #     error_estimates_res_std[:, 0] ** 2,
+    #     color="black",
+    # )
+    # ax[i].semilogy(
+    #     t,
+    #     true_error ** 2,
+    #     color=reference_color,
+    #     linestyle=reference_linestyle,
+    # )
+    # ax[i].set_ylim(LOG_YLIM)
+    # ax[i].set_ylabel("$\\log \\mathbb{V}[\\delta(Y)](t)$", fontsize="x-small")
     # ax[i].set_title(
     #     f"$\\bf {titles[i]}$", loc="left", fontweight="bold", fontsize="x-small"
     # )
+    # ax[i].axhline(TOL, color=reference_color)
     # i = next(idx)
     #
-    # # # Seventh row: steps from refinement with residual only
-    # # for t in grid_refine_via_prob_res:
-    # #     ax[i].axvline(t, color="black", linewidth=0.5)
+    # # Just the STD.
+    # ax[i].semilogy(
+    #     t,
+    #     error_estimates_res_std[:, 0] ** 2,
+    #     color="black",
+    # )
+    # # # ax[i].semilogy(
+    # # #     t,
+    # # #     error_estimates_res_mean[:, 0] + error_estimates_res_std[:, 0],
+    # # #     color="black",
+    # # #     linestyle="dashed",
+    # # # )
+    # ax[i].semilogy(
+    #     t,
+    #     error_estimates_res_std[:, 0] ** 2,
+    #     color="black",
+    # )
+    # ax[i].semilogy(
+    #     t,
+    #     true_error ** 2,
+    #     color=reference_color,
+    #     linestyle=reference_linestyle,
+    # )
+    # ax[i].set_ylim(LOG_YLIM)
+    # ax[i].set_ylabel("$\\log \\|\\delta(Y)(t)\\|$", fontsize="x-small")
+    # ax[i].set_title(
+    #     f"$\\bf {titles[i]}$", loc="left", fontweight="bold", fontsize="x-small"
+    # )
+    # ax[i].axhline(TOL, color=reference_color)
+    # i = next(idx)
+    #
+    # # Sixth row: steps from refinement with residual only
+    # # for t in grid_refine_via_res:
+    # #     ax[i].axvline(t, color="black", linewidth=0.25)
     # # ax[i].set_title(
     # #     f"$\\bf {titles[i]}$", loc="left", fontweight="bold", fontsize="x-small"
     # # )
     # # i = next(idx)
+    # #
+    # # # # Seventh row: steps from refinement with residual only
+    # # # for t in grid_refine_via_prob_res:
+    # # #     ax[i].axvline(t, color="black", linewidth=0.5)
+    # # # ax[i].set_title(
+    # # #     f"$\\bf {titles[i]}$", loc="left", fontweight="bold", fontsize="x-small"
+    # # # )
+    # # # i = next(idx)
 
     # Clean up: remove all ticks for now and show the plot
     for a in ax:
@@ -295,5 +313,5 @@ for N in [2**5, 2**7, 2**9]:
         a.set_yticks([])
         a.set_xlim((bvp.t0, bvp.tmax))
 
-    plt.show()
+    plt.savefig(f"errorestimates{N}.pdf")
 
