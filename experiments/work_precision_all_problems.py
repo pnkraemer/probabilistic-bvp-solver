850fc195d5b0e7d6864d435a1e3ca48d2ece363e
Nicholas Krämer <nicokraemer@gmx.net>
2021-05-19 11:45:59 +0200

    something looks okay

--- experiments/work_precision_all_problems.py
+++ experiments/work_precision_all_problems.py
@@ -11,7 +11,7 @@ from scipy.integrate import solve_bvp
 from bvps import bvp_solver, problem_examples
 
 # Load problems
-XI = 0.01
+XI = 0.02
 bvp = problem_examples.problem_7_second_order(xi=XI)
 bvp1st = problem_examples.problem_7(xi=XI)
 
@@ -19,7 +19,7 @@ bvp1st = problem_examples.problem_7(xi=XI)
 initial_grid = np.linspace(bvp.t0, bvp.tmax, 10)
 initial_guess = np.zeros((bvp1st.dimension, len(initial_grid)))
 refsol_fine = solve_bvp(
-    bvp1st.f, bvp1st.scipy_bc, initial_grid, initial_guess, tol=1e-4
+    bvp1st.f, bvp1st.scipy_bc, initial_grid, initial_guess, tol=1e-7
 )
 assert refsol_fine.success
 bvp.solution = refsol_fine.sol
@@ -30,8 +30,8 @@ results = {}
 testlocations = np.linspace(bvp.t0, bvp.tmax, 400)
 
 
-ORDERS = [3, 4, 5]
-TOLERANCES = [1e-1, 1e-2, 1e-3, 1e-4, 1e-5]
+ORDERS = list(reversed([3, 4, 5]))
+TOLERANCES = list(reversed([1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6]))
 
 for q in tqdm.tqdm(ORDERS):
 
@@ -43,46 +43,107 @@ for q in tqdm.tqdm(ORDERS):
         # Set up dictionary for the results for given tolerance
         results[q][tol] = {}
 
-        # Solve the BVP
-        ibm = statespace.IBM(
-            ordint=q,
-            spatialdim=bvp.dimension,
-            forward_implementation="sqrt",
-            backward_implementation="sqrt",
-        )
-        solver = bvp_solver.BVPSolver.from_default_values_std_refinement(
-            ibm, initial_sigma_squared=1e11
-        )
-        initial_posterior, _ = solver.compute_initialisation(
-            bvp, initial_grid, initial_guess=None, use_bridge=True
-        )
+        # The preferable way is to solve the BVP with the bridge prior.
+        # If that does not work, use a manual initial guess.
+        # If that does not work either, set NaNs.
+        try:
 
-        solution_gen = solver.solution_generator(
-            bvp,
-            atol=tol,
-            rtol=tol,
-            initial_posterior=initial_posterior,
-            maxit_ieks=1,
-            maxit_em=1,
-            yield_ieks_iterations=False,
-        )
-        start_time = time.time()
-        for kalman_posterior, sigma_squared in solution_gen:
-            pass
-        runtime = time.time() - start_time
+            # Solve the BVP
+            ibm = statespace.IBM(
+                ordint=q,
+                spatialdim=bvp.dimension,
+                forward_implementation="sqrt",
+                backward_implementation="sqrt",
+            )
+            solver = bvp_solver.BVPSolver.from_default_values_std_refinement(
+                ibm, initial_sigma_squared=1e11
+            )
+            initial_posterior, _ = solver.compute_initialisation(
+                bvp, initial_grid, initial_guess=None, use_bridge=True
+            )
 
-        # Transform solution into something more convenient
-        solution = diffeq.KalmanODESolution(kalman_posterior)
-        solution_mean = lambda *args: solution(*args).mean
+            solution_gen = solver.solution_generator(
+                bvp,
+                atol=tol,
+                rtol=tol,
+                initial_posterior=initial_posterior,
+                maxit_ieks=1,
+                maxit_em=1,
+                yield_ieks_iterations=False,
+            )
+            start_time = time.time()
 
-        # Compute error and calibration
-        chi2 = (
-            timeseries.anees(solution, reference_solution, testlocations)
-            / sigma_squared
-        )
-        error = timeseries.root_mean_square_error(
-            solution_mean, reference_solution, testlocations
-        )
+            for kalman_posterior, sigma_squared in solution_gen:
+                pass
+            runtime = time.time() - start_time
+
+            # Transform solution into something more convenient
+            solution = diffeq.KalmanODESolution(kalman_posterior)
+            solution_mean = lambda *args: solution(*args).mean
+
+            # Compute error and calibration
+            chi2 = (
+                    timeseries.anees(solution, reference_solution,
+                                     testlocations)
+                    / sigma_squared
+            )
+            error = timeseries.root_mean_square_error(
+                solution_mean, reference_solution, testlocations
+            )
+            N = len(solution.locations)
+        except ValueError:
+            try:
+
+                # Solve the BVP
+                ibm = statespace.IBM(
+                    ordint=q,
+                    spatialdim=bvp.dimension,
+                    forward_implementation="sqrt",
+                    backward_implementation="sqrt",
+                )
+                solver = bvp_solver.BVPSolver.from_default_values_std_refinement(
+                    ibm, initial_sigma_squared=1e11
+                )
+                initial_posterior, _ = solver.compute_initialisation(
+                    bvp, initial_grid, initial_guess=initial_guess[0][:, None], use_bridge=True
+                )
+
+                solution_gen = solver.solution_generator(
+                    bvp,
+                    atol=tol,
+                    rtol=tol,
+                    initial_posterior=initial_posterior,
+                    maxit_ieks=1,
+                    maxit_em=1,
+                    yield_ieks_iterations=False,
+                )
+                start_time = time.time()
+
+                for kalman_posterior, sigma_squared in solution_gen:
+                    pass
+                runtime = time.time() - start_time
+
+                # Transform solution into something more convenient
+                solution = diffeq.KalmanODESolution(kalman_posterior)
+                solution_mean = lambda *args: solution(*args).mean
+
+                # Compute error and calibration
+                chi2 = (
+                        timeseries.anees(solution, reference_solution,
+                                         testlocations)
+                        / sigma_squared
+                )
+                error = timeseries.root_mean_square_error(
+                    solution_mean, reference_solution, testlocations
+                )
+                N = len(solution.locations)
+            except ValueError:
+
+                runtime =np.nan
+                chi2 = np.nan
+                error = np.nan
+                N = np.nan
+
 
         # How fast would scipy be?
         start_time_scipy = time.time()
@@ -90,6 +151,7 @@ for q in tqdm.tqdm(ORDERS):
             bvp1st.f, bvp1st.scipy_bc, initial_grid, initial_guess, tol=tol
         )
         runtime_scipy = time.time() - start_time_scipy
+        assert scipy_solution.success
 
         # How accurate would scipy be?
         scipy_sol_for_rmse = lambda *args: scipy_solution.sol(*args)[0][:, None]
@@ -101,7 +163,7 @@ for q in tqdm.tqdm(ORDERS):
         results[q][tol]["chi2"] = chi2
         results[q][tol]["time"] = runtime
         results[q][tol]["error"] = error
-        results[q][tol]["N"] = len(solution.locations)
+        results[q][tol]["N"] = N
         results[q][tol]["time_scipy"] = runtime_scipy
         results[q][tol]["error_scipy"] = error_scipy
         results[q][tol]["N_scipy"] = len(scipy_solution.x)
