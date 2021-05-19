3f63de0db8b20cf6ce63733e1994674066733799
Nicholas Krämer <nicokraemer@gmx.net>
2021-05-19 10:59:49 +0200

    problem7 workprecision done

--- experiments/work_precision_all_problems.py
+++ experiments/work_precision_all_problems.py
@@ -27,11 +27,12 @@ reference_solution = lambda *args: bvp.solution(*args)[0][:, None]
 
 # Set up empty dictionary and determine the locations for computation of anees and rmse
 results = {}
-testlocations = np.linspace(bvp.t0, bvp.tmax, 5)
+testlocations = np.linspace(bvp.t0, bvp.tmax, 400)
 
 
-ORDERS = reversed([3, 4, 5, 6])
-TOLERANCES = reversed([1e-1, 1e-2, 1e-3, 1e-4, 1e-5])
+ORDERS = [3, 4, 5]
+TOLERANCES = [1e-1, 1e-2, 1e-3, 1e-4, 1e-5]
+
 for q in tqdm.tqdm(ORDERS):
 
     # Set up dictionary for the results for given q
@@ -45,12 +46,12 @@ for q in tqdm.tqdm(ORDERS):
         # Solve the BVP
         ibm = statespace.IBM(
             ordint=q,
-            spatialdim=1,
+            spatialdim=bvp.dimension,
             forward_implementation="sqrt",
             backward_implementation="sqrt",
         )
         solver = bvp_solver.BVPSolver.from_default_values_std_refinement(
-            ibm, initial_sigma_squared=1e8
+            ibm, initial_sigma_squared=1e11
         )
         initial_posterior, _ = solver.compute_initialisation(
             bvp, initial_grid, initial_guess=None, use_bridge=True
@@ -83,11 +84,27 @@ for q in tqdm.tqdm(ORDERS):
             solution_mean, reference_solution, testlocations
         )
 
+        # How fast would scipy be?
+        start_time_scipy = time.time()
+        scipy_solution = solve_bvp(
+            bvp1st.f, bvp1st.scipy_bc, initial_grid, initial_guess, tol=tol
+        )
+        runtime_scipy = time.time() - start_time_scipy
+
+        # How accurate would scipy be?
+        scipy_sol_for_rmse = lambda *args: scipy_solution.sol(*args)[0][:, None]
+        error_scipy = timeseries.root_mean_square_error(scipy_sol_for_rmse, reference_solution, testlocations)
+
+
+
         # Save results
-        results[q][tol]["time"] = runtime
         results[q][tol]["chi2"] = chi2
+        results[q][tol]["time"] = runtime
         results[q][tol]["error"] = error
         results[q][tol]["N"] = len(solution.locations)
+        results[q][tol]["time_scipy"] = runtime_scipy
+        results[q][tol]["error_scipy"] = error_scipy
+        results[q][tol]["N_scipy"] = len(scipy_solution.x)
 
 print(results)
 
