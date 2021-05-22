cc1736a02157d93944355ae8e3f0fe9c77fee983
Nicholas Krämer <nicokraemer@gmx.net>
2021-05-22 07:55:24 +0200

    attempt to write down an ODE measmod

--- bvps/ode_measmods.py
+++ bvps/ode_measmods.py
@@ -6,7 +6,7 @@ import scipy.linalg
 from probnum import filtsmooth, statespace
 from probnum._randomvariablelist import _RandomVariableList
 
-from .problems import SecondOrderBoundaryValueProblem
+from .problems import SecondOrderBoundaryValueProblem, FourthOrderBoundaryValueProblem
 
 
 def from_ode(ode, prior, damping_value=0.0):
@@ -83,6 +83,8 @@ def from_second_order_ode(ode, prior, damping_value=0.0):
 
 def from_boundary_conditions(bvp, prior, damping_value=0.0):
 
+    if isinstance(bvp, FourthOrderBoundaryValueProblem):
+        return from_boundary_conditions_fourth_order(bvp, prior, damping_value=damping_value)
     if isinstance(bvp, SecondOrderBoundaryValueProblem):
         P0 = prior.proj2coord(0)
         P1 = prior.proj2coord(1)
@@ -154,3 +156,49 @@ def from_fourth_order_ode(ode, prior, damping_value=0.0):
         forward_implementation="sqrt",
         backward_implementation="sqrt",
     )
+
+
+def from_boundary_conditions_fourth_order(bvp, prior, damping_value=0.0):
+
+    P0 = prior.proj2coord(0)
+    P1 = prior.proj2coord(1)
+    P2 = prior.proj2coord(2)
+    P3 = prior.proj2coord(3)
+    proj = np.vstack((P0, P1, P2, p3))
+
+    use_y0 = (bvp.y0 is not None)
+    use_dy0 = (bvp.dy0 is not None)
+    use_ddy0 = (bvp.ddy0 is not None)
+    use_dddy0 = (bvp.dddy0 is not None)
+    use_ymax = (bvp.ymax is not None)
+    use_dymax = (bvp.dymax is not None)
+    use_ddymax = (bvp.ddymax is not None)
+    use_dddymax = (bvp.dddymax is not None)
+
+    zeros = np.zeros((bvp.dimension, bvp.dimension))
+    Ls = (Li if use else zeros for Li, use in zip([bvp.L_y,bvp.L_dy,bvp.L_ddy,bvp.L_dddy], [use_y0,use_dy0,use_ddy0,use_dddy0]))
+    Rs = (Ri if use else zeros for Ri, use in zip([bvp.R_y,bvp.R_dy,bvp.R_ddy,bvp.R_dddy], [use_ymax,use_dymax,use_ddymax,use_dddymax]))
+
+    L = np.vstack(tuple(Ls))
+    R = np.vstack(tuple(Rs))
+
+    Rnew = R @ proj
+    Lnew = L @ proj
+
+    measmod_R = statespace.DiscreteLTIGaussian(
+        Rnew,
+        -bvp.ymax,
+        damping_value * np.eye(len(R)),
+        proc_noise_cov_cholesky=np.sqrt(damping_value) * np.eye(len(R)),
+        forward_implementation="sqrt",
+        backward_implementation="sqrt",
+    )
+    measmod_L = statespace.DiscreteLTIGaussian(
+        Lnew,
+        -bvp.y0,
+        damping_value * np.eye(len(L)),
+        proc_noise_cov_cholesky=np.sqrt(damping_value) * np.eye(len(L)),
+        forward_implementation="sqrt",
+        backward_implementation="sqrt",
+    )
+    return measmod_L, measmod_R
